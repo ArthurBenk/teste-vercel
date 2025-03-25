cffd729428c2e3067f0c0380427b1b55f68a8e1a
ArthurBenk <arthur.benk.ifc@gmail.com>
2025-03-25 09:04:45 -0300

    vercel-test

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from "vue";
+
+const numero= ref(0)
+function somar() {
+  numero.value++  
+}
+</script>
+
+<template>
+<h1>Vercel teste</h1>
+<p>{{ numero }}</p>
+<button @click="somar()">+</button>
+</template>
+
+<style scoped>
+
+</style>
